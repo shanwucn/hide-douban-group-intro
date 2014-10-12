2edabc02d168d84d157159f2306e120ac30d6e76
shawnwu <Shawn@Shawns-MacBook-Air.local>
2014-10-12 18:10:42 +0800

    Hide Douban Group Intro for Group Working Holiday

---
+++ remove_douban_group_intro.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name Remove Douban Group Intro
+// @namespace remove_douban_group_intro
+// @include http://www.douban.com/group/WHV/
+// @version 1.0
+// ==/UserScript==
+
+GM_addStyle("div.group-intro { display: none !important; }"); 
